4ccf6b647b1f5a2abdf117f6b9c5a3dd9d41723e
Vitor Santos <69792090+VitorSantos920@users.noreply.github.com>
2021-09-05 02:54:34 -0300

    Update bd.php

--- bd/bd.php
+++ bd/bd.php
@@ -1,1 +1,11 @@
-
+<?php
+// Conexão com o Banco de Dados usando mysqli_connect()
+    $conexao = mysqli_connect(
+        "localhost",
+        "root",
+        "",
+        "php_project"
+    );
+// Definindo os caracteres como UTF-8
+    mysqli_set_charset($conexao, "utf8");
+?>
